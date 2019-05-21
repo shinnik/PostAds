8dcbf293f4bcdeaa91e592fae095631bf9720e44
Nikita Izrailev <izrailev@phystech.edu>
2019-05-21 18:44:27 +0300

    Updates

---
+++ precache-manifest.e2e83739e4d55e190bb82ca8b0dc4e9b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c5eac54275f737d2cd6a565e994bf8e",
+    "url": "/PostAds/index.html"
+  },
+  {
+    "revision": "d474dd6b66d7c1b40aec",
+    "url": "/PostAds/static/css/main.65dcae34.chunk.css"
+  },
+  {
+    "revision": "f462cd5ff5e6d3f499b4",
+    "url": "/PostAds/static/js/2.bf5fc167.chunk.js"
+  },
+  {
+    "revision": "d474dd6b66d7c1b40aec",
+    "url": "/PostAds/static/js/main.fa144c4b.chunk.js"
+  },
+  {
+    "revision": "7cfb6e3045be95d5eea8",
+    "url": "/PostAds/static/js/runtime~main.6547c13c.js"
+  },
+  {
+    "revision": "cc6ab263dc10b893c41459a4898bef9f",
+    "url": "/PostAds/static/media/arrow-down.cc6ab263.svg"
+  },
+  {
+    "revision": "d6858d4a5a9305720d10e045c43ddc11",
+    "url": "/PostAds/static/media/erase.d6858d4a.svg"
+  },
+  {
+    "revision": "7195a9634e77464bfa443a8e866848db",
+    "url": "/PostAds/static/media/info.7195a963.svg"
+  },
+  {
+    "revision": "e06d0a8458c59d059a48eb1e074c4a1f",
+    "url": "/PostAds/static/media/invalid.e06d0a84.svg"
+  },
+  {
+    "revision": "4e914679a1e612cd37749232b1d13a11",
+    "url": "/PostAds/static/media/location.4e914679.svg"
+  },
+  {
+    "revision": "2ffbaf4d3ec7739f240d7c104089a997",
+    "url": "/PostAds/static/media/phone.2ffbaf4d.svg"
+  },
+  {
+    "revision": "2151cce6cc6aae7215da5f4763039955",
+    "url": "/PostAds/static/media/valid.2151cce6.svg"
+  }
+]);
